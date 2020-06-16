66eb5d97cda6d2309e7912229dcb8b82bd5637ec
Matthew Martin <martim01@outlook.com>
2020-06-16 16:10:44 +0100

    build: version updates

--- player3/include/version.h
+++ player3/include/version.h
@@ -5,9 +5,10 @@ namespace version
     static const unsigned long MAJOR=0;
     static const unsigned long MINOR=1;
     static const unsigned long PATCH=0;
-    static const unsigned long BUILD=34;
-    static const unsigned long DATE=1592233727;
+    static const unsigned long BUILD=35;
+    static const unsigned long DATE=1592296055;
 };
+
 
 
 
