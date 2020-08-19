0e574a255c933ef09afa664bdfd36eee988959b1
Matthew Martin <martim01@outlook.com>
2020-08-19 16:44:31 +0100

    improvement: shrink size of card on lower resolution

--- loopi/index.php
+++ loopi/index.php
@@ -37,7 +37,7 @@ $loopi_array = loadloopi();
         for($i = 0; $i < count($loopi_array); $i++)
         {
             echo "<div>\n";
-            echo "<a class='uk-link-reset uk-display-block uk-width-large@l uk-width-1-3@s uk-card uk-card-default uk-card-body uk-card-hover uk-card-small' id='loopi_$i'  href='status/?loopi=".$loopi_array[$i]."'>\n";
+            echo "<a class='uk-link-reset uk-display-block uk-width-large@l uk-width-medium@m uk-card uk-card-default uk-card-body uk-card-hover uk-card-small' id='loopi_$i'  href='status/?loopi=".$loopi_array[$i]."'>\n";
             echo "<div class='uk-card-header'>\n";
             echo "<h3 class='uk-card-title'><span id='host_$i'>".$loopi_array[$i]."</span></h3>\n";
             echo "</div>\n";
@@ -70,7 +70,7 @@ $loopi_array = loadloopi();
         }
         ?>
             <div  id="loopi_add">
-            <div class="uk-width-large uk-card uk-card-default uk-card-body uk-card-hover uk-card-small" onclick="addLoopi()">
+            <div class="uk-width-large@l uk-width-medium@m uk-card uk-card-default uk-card-body uk-card-hover uk-card-small" onclick="addLoopi()">
                <div class='uk-card-header'><h3 class="uk-card-title">Add loopi</h3></div>
                <div class='uk-card-body uk-background-norepeat uk-background-center-center' style="background-image: url(images/add.png)"><br/><br/><br/><br/><br/><br/></div>
                <div class='uk-card-footer'></div>
