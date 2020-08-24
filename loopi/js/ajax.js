e105864d218105e1968d5457569ef67af1b151bb
Matthew Martin <martim01@outlook.com>
2020-08-24 15:54:00 +0100

    features: more work on web interface

--- loopi/js/ajax.js
+++ loopi/js/ajax.js
@@ -10,6 +10,8 @@ class loopi
 		this.playlists = [];
 		this.schedules = [];
 		this.connected = false;
+		
+		this.expanded = false;
 	}
 	
 	setWebsocket(ws)
@@ -54,7 +56,6 @@ class loopi
 	
 	findFile(uid)
 	{
-		console.log(this.files);
 		for(var i = 0; i < this.files.length; i++)
 		{
 			if(this.files[i]['uid'] == uid)
@@ -65,6 +66,29 @@ class loopi
 		return -1;
 	}
 	
+	findPlaylist(uid)
+	{
+		for(var i = 0; i < this.playlists.length; i++)
+		{
+			if(this.playlists[i]['uid'] == uid)
+			{
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	findSchedule(uid)
+	{
+		for(var i = 0; i < this.schedules.length; i++)
+		{
+			if(this.schedules[i]['uid'] == uid)
+			{
+				return i;
+			}
+		}
+		return -1;
+	}
 }
 
 
@@ -96,7 +120,7 @@ function getLoopiConfig_Dashboard(loopi)
 	}
 	else
 	{
-		doNextLoopi(loopi);
+		doNextLoopi_Dashboard(loopi);
 	}
 }
 
@@ -109,7 +133,7 @@ function handleConfig_Dashboard(loopi, jsonObj)
 	}
 	else
 	{
-		doNextLoopi(loopi);
+		doNextLoopi_Dashboard(loopi);
 	}
 }
 
@@ -121,6 +145,13 @@ function handleStatus_Dashboard(loopi, jsonObj)
 
 }
 
+function doNextLoopi(loopi)
+{
+	loopi++;
+	if(loopi < g_loopi_array.length)
+	{
+	}
+}
 
 
 
@@ -128,7 +159,7 @@ function storeFiles_Dashboard(loopi, jsonObj)
 {
 	g_loopi_array[loopi].setFiles(jsonObj);
 	ws_connect(loopi, updateStatus_Dashboard);
-	doNextDashboardLoopi(loopi);
+	doNextLoopi_Dashboard(loopi);
 }
 
 
@@ -157,7 +188,7 @@ function ws_connect(loopi, statusCallback)
 
 
 
-function doNextDashboardLoopi(loopi)
+function doNextLoopi_Dashboard(loopi)
 {
 	loopi++;
 	if(loopi < g_loopi_array.length)
@@ -338,6 +369,7 @@ function ajaxGet(loopi, endpoint, callback)
 			if(this.status === 200)
 			{
 				g_loopi_array[loopi].connected = true;
+
 				callback(loopi, JSON.parse(this.responseText));
 			}
 			else
@@ -563,9 +595,422 @@ function handleStatus_Details(loopi, jsonObj)
 function storeFiles_Details(loopi, jsonObj)
 {
 	g_loopi_array[loopi].setFiles(jsonObj);
+	
+	listResource_Details(jsonObj, "files", getFileDetails);
+	
+	getPlaylists(loopi, storePlaylists_Details);
+}
+
+
+function storePlaylists_Details(loopi, jsonObj)
+{
+	g_loopi_array[loopi].setPlaylists(jsonObj);
+	
+	listResource_Details(jsonObj, "playlists", getPlaylistDetails);
+	
+	getSchedules(loopi, storeSchedules_Details);
+}
+
+function storeSchedules_Details(loopi, jsonObj)
+{
+	g_loopi_array[loopi].setSchedules(jsonObj);
+	
+	listResource_Details(jsonObj, "schedules", getScheduleDetails);
+	
 	ws_connect(loopi, updateStatus_Details);
 	
 }
+
+
+function listResource_Details(jsonObj, type, clickFunction)
+{
+	var ul = document.getElementById('resource_'+type);
+	clearElement(ul);
+	
+	
+	var resObj = jsonObj;
+	resObj.sort(function(a,b) { 
+								var x = a.label.toLowerCase();
+								var y = b.label.toLowerCase();
+								if(x < y) return -1;
+								if(y < x) return 1;
+								return 0;
+							});
+	
+		
+	for(var i = 0; i < resObj.length; i++)
+	{
+		var li = document.createElement('li');
+		li.classList.add('uk-margin-small-top');
+		li.style.padding = '4px 6px';
+		
+		var a = document.createElement('a');
+		a.className = 'uk-accordion-title';
+		a.id = resObj[i]['uid'];
+		a.addEventListener('click', clickFunction, false);
+		a.href='#';
+		
+		var div_label = document.createElement('div');
+		div_label.style.float = 'left';
+		div_label.style.width = '60%';
+		
+		var span = document.createElement('span');
+		span.innerHTML = resObj[i]['label'];
+		div_label.appendChild(span);
+		
+		var div_action = document.createElement('div');
+		div_action.style.float = 'left';
+		div_action.style.width = '20%';
+		
+		var button = document.createElement('button');
+		button.className = "uk-button";
+		button.classList.add("uk-button-primary");
+		button.classList.add("uk-button-small");
+		button.id = "play_"+type+"_"+resObj[i]["uid"];
+		button.innerHTML = 'Play';
+		button.addEventListener('click',play, false);
+		div_action.appendChild(button);
+		
+		
+		a.appendChild(div_label);
+		a.appendChild(div_action);
+		
+		li.appendChild(a);
+		
+			
+		var div_content = document.createElement('div');
+		div_content.className = "uk-accordion-content";
+		
+		
+		var tableResource = document.createElement('table');
+		tableResource.className = 'uk-table';
+		tableResource.classList.add('uk-table-small');
+		//tableResource.style.border = 'solid 2px #c8c8f8';
+		
+		var tbodyResourceDetails = document.createElement('tbody');
+		tbodyResourceDetails.id = 'details_'+resObj[i]["uid"];
+		
+		tableResource.appendChild(tbodyResourceDetails);
+		div_content.appendChild(tableResource);
+		
+		var tableSpecific = document.createElement('table');
+		tableSpecific.className = 'uk-table';
+		tableSpecific.classList.add('uk-table-small');
+		if(type != 'files')
+		{
+			tableSpecific.classList.add('uk-table-divider');
+		}
+		
+		var theadspecific = document.createElement('thead');
+		theadspecific.id = 'specifichead_'+resObj[i]["uid"];
+		
+		var tbodySpecificDetails = document.createElement('tbody');
+		tbodySpecificDetails.id = 'specific_'+resObj[i]["uid"];
+		
+		tableSpecific.appendChild(theadspecific);
+		tableSpecific.appendChild(tbodySpecificDetails);
+		div_content.appendChild(tableSpecific);
+		
+		
+		li.appendChild(div_content);
+		
+		ul.appendChild(li);
+	}
+}
+
+
+function getFileDetails(e)
+{
+	var target = e.target;
+	while(target.tagName != 'A' && target.tagName != 'a')
+	{
+		target = target.parentElement;
+		console.log(target.tagName);
+	}
+	
+	getFile(0, target.id, createFileDetails);
+}
+
+function getPlaylistDetails(e)
+{
+	var target = e.target;
+	while(target.tagName != 'A' && target.tagName != 'a')
+	{
+		target = target.parentElement;
+		console.log(target.tagName);
+	}
+	getPlaylist(0, target.id, createPlaylistDetails);
+}
+
+function getScheduleDetails(e)
+{
+	var target = e.target;
+	while(target.tagName != 'A' && target.tagName != 'a')
+	{
+		target = target.parentElement;
+		console.log(target.tagName);
+	}
+	getSchedule(0, target.id, createScheduleDetails);
+}
+
+
+function createDetailsRow(key, value)
+{
+	var tr = document.createElement('tr');
+	var td_key = document.createElement('td');
+	td_key.style.padding = '3px 6px';
+	
+	var span = document.createElement('span');
+	span.className = 'uk-text-bold';
+	span.innerHTML = key;
+	
+	td_key.appendChild(span);
+	
+	
+	var td_value = document.createElement('td');
+	td_value.innerHTML = value;
+	td_value.style.padding = '3px 6px';
+	
+	tr.appendChild(td_key);
+	tr.appendChild(td_value);
+	
+	return tr;
+}
+
+function createResourceDetails(jsonObj)
+{
+	var tbody = document.getElementById('details_'+jsonObj["uid"]);
+	if(tbody)
+	{
+		clearElement(tbody);
+		
+		tbody.appendChild(createDetailsRow('UID', jsonObj['uid']));
+		tbody.appendChild(createDetailsRow('Description', jsonObj['description']));
+		tbody.appendChild(createDetailsRow('Created', jsonObj['created']));
+		if(jsonObj['modified'] !== undefined)
+		{
+			tbody.appendChild(createDetailsRow('Modified', jsonObj['modified']));
+		}
+		
+		if(jsonObj['file_modified'] !== undefined)
+		{
+			tbody.appendChild(createDetailsRow('File Modified', jsonObj['file_modified']));
+		}
+		tbody.appendChild(createDetailsRow('Locked', jsonObj['locked']));
+	}
+	return tbody;
+}
+
+function createFileDetails(loopi, jsonObj)
+{
+	createResourceDetails(jsonObj);
+	var tbody = document.getElementById('specific_'+jsonObj["uid"]);
+	if(tbody)
+	{
+		clearElement(tbody);
+		tbody.appendChild(createFormatRow('Channels', jsonObj['channels'], 'Sample Rate', jsonObj['samplerate']));
+		tbody.appendChild(createFormatRow('Type', jsonObj["format"]['type'], "Format", jsonObj["format"]['subtype']));
+		tbody.appendChild(createDetailsRow('Length', millisecondsToTime(jsonObj['length'], '&nsbp;', '&nsbp;')));	
+	}
+}
+
+
+function createFormatRow(key1, value1, key2, value2)
+{
+	var tr = document.createElement('tr');
+	var td_key1 = document.createElement('td');
+	td_key1.style.padding = '3px 6px';
+	
+	var span1 = document.createElement('span');
+	span1.className = 'uk-text-bold';
+	span1.innerHTML = key1;
+	
+	td_key1.appendChild(span1);
+	
+	
+	var td_value1 = document.createElement('td');
+	td_value1.innerHTML = value1;
+	td_value1.style.padding = '3px 6px';
+	
+	var td_key2 = document.createElement('td');
+	td_key2.style.padding = '3px 6px';
+	
+	var span2 = document.createElement('span');
+	span2.className = 'uk-text-bold';
+	span2.innerHTML = key2;
+	
+	td_key2.appendChild(span2);
+	
+	
+	var td_value2 = document.createElement('td');
+	td_value2.innerHTML = value2;
+	td_value2.style.padding = '3px 6px';
+	
+	
+	
+	tr.appendChild(td_key1);
+	tr.appendChild(td_value1);
+	tr.appendChild(td_key2);
+	tr.appendChild(td_value2);
+	
+	return tr;
+}
+
+function createPlaylistDetails(loopi, jsonObj)
+{
+	createResourceDetails(jsonObj);
+	
+	var thead = document.getElementById('specifichead_'+jsonObj["uid"]);
+	clearElement(thead);
+	
+	var thFile = document.createElement('th');
+	thFile.innerHTML = 'File';
+	
+	var thTime = document.createElement('th');
+	thTime.innerHTML = 'Times To Play';
+	thead.appendChild(thFile);
+	thead.appendChild(thTime);
+	
+	
+	var tbody = document.getElementById('specific_'+jsonObj["uid"]);
+	if(tbody)
+	{
+		clearElement(tbody);
+		for(var i = 0; i < jsonObj['files'].length; i++)
+		{
+			var file = g_loopi_array[loopi].findFile(jsonObj['files'][i]['uid']);
+			if(file > -1)
+			{
+				tbody.appendChild(createDetailsRow(g_loopi_array[loopi].files[file]['label'], jsonObj['files'][i]['times_to_play']));
+			}
+			else
+			{
+				tbody.appendChild(createDetailsRow(jsonObj['files'][i]['uid'], jsonObj['files'][i]['times_to_play']));
+			}
+		}
+	}
+}
+
+function createScheduleDetails(loopi, jsonObj)
+{
+	createResourceDetails(jsonObj);
+	
+	var thead = document.getElementById('specifichead_'+jsonObj["uid"]);
+	clearElement(thead);
+	
+	var thType = document.createElement('th');
+	thType.innerHTML = 'Type';
+	
+	var thFile = document.createElement('th');
+	thFile.innerHTML = 'Label';
+	
+	var thTime = document.createElement('th');
+	thTime.innerHTML = 'Times To Play';
+	
+	var thS = document.createElement('th');
+	thS.innerHTML = 'Sec';
+	var thM = document.createElement('th');
+	thM.innerHTML = 'Min';
+	var thH = document.createElement('th');
+	thH.innerHTML = 'Hr';
+	var thD = document.createElement('th');
+	thD.innerHTML = 'DoM';
+	var thMo = document.createElement('th');
+	thMo.innerHTML = 'Mon';
+	var thDoW = document.createElement('th');
+	thDoW.innerHTML = 'DoW';
+	var thY = document.createElement('th');
+	thY.innerHTML = 'Year';
+	
+	thead.appendChild(thType);
+	thead.appendChild(thFile);
+	thead.appendChild(thTime);
+	thead.appendChild(thS);
+	thead.appendChild(thM);
+	thead.appendChild(thH);
+	thead.appendChild(thD);
+	thead.appendChild(thMo);
+	thead.appendChild(thDoW);
+	thead.appendChild(thY);
+	
+	
+	var tbody = document.getElementById('specific_'+jsonObj["uid"]);
+	if(tbody)
+	{
+		clearElement(tbody);
+		createScheduleResourceDetails(tbody, 'file',jsonObj);
+		createScheduleResourceDetails(tbody, 'playlist',jsonObj);
+		
+	}
+}
+
+function createScheduleResourceDetails(tbody,type, jsonObj)
+{
+	var res = type+'s';
+	for(var i = 0; i < jsonObj[res].length; i++)
+	{
+		var file = -1;
+		var label = jsonObj[res][i]['uid'];
+		
+		if(type == 'file')
+		{
+			file = g_loopi_array[0].findFile(jsonObj[res][i]['uid']);
+			if(file > -1)
+			{
+				label = g_loopi_array[0].files[file]['label'];
+			}
+		}
+		else
+		{
+			file = g_loopi_array[0].findPlaylist(jsonObj[res][i]['uid']);
+			if(file > -1)
+			{
+				label = g_loopi_array[0].playlists[file]['label'];
+			}
+		}
+		
+		
+		tbody.appendChild(createScheduleDetailsRow(type, label, jsonObj[res][i]['times_to_play'], jsonObj[res][i]['cron']));
+	}
+}
+
+function createScheduleDetailsRow(type, label, loop, cron)
+{
+
+	var tr = document.createElement('tr');
+	
+	var td_type= document.createElement('td');
+	td_type.style.padding = '3px 6px';
+	td_type.innerHTML = type;
+	
+		
+	var td_label = document.createElement('td');
+	td_label.innerHTML = label;
+	td_label.style.padding = '3px 6px';
+	
+	var td_loop = document.createElement('td');
+	td_loop.innerHTML = loop;
+	td_loop.style.padding = '3px 6px';
+	
+	tr.appendChild(td_type);
+	tr.appendChild(td_label);
+	tr.appendChild(td_loop);
+	
+	var split = cron.split(' ');
+	for(var i = 0; i < split.length; i++)
+	{
+		if(split[i] != '')
+		{
+			var td_cron = document.createElement('td');
+			td_cron.innerHTML = split[i];
+			td_cron.style.padding = '3px 6px';
+			tr.appendChild(td_cron);
+		}
+	}
+	
+	return tr;
+}
+
 
 function updateConfig_Details(loopi, jsonObj)
 {
@@ -621,6 +1066,7 @@ function updateStatus_Details(loopi, jsonObj)
 		document.getElementById("player").classList.remove("uk-label-warning");
 		document.getElementById("player").classList.add("uk-label-danger");
 		document.getElementById("section_player").style.display = "none";
+		document.getElementById("section_resources").style.display = "block";
 		
 		document.getElementById("button_stop").style.visibility = "hidden";
 		
@@ -634,6 +1080,8 @@ function updateStatus_Details(loopi, jsonObj)
 		document.getElementById("player").classList.remove("uk-label-danger");
 		
 		document.getElementById("section_player").style.display = "none";
+		document.getElementById("section_resources").style.display = "block";
+		
 		document.getElementById("card_resource").style.visibility = "hidden";
 		document.getElementById("card_playout").style.visibility = "hidden";
 		
@@ -649,6 +1097,8 @@ function updateStatus_Details(loopi, jsonObj)
 		showStopButton(loopi);
 		
 		document.getElementById("section_player").style.display = "block";
+		document.getElementById("section_resources").style.display = "none";
+		
 		document.getElementById("card_resource").style.visibility = "visible";
 		
 		document.getElementById('resource_type').innerHTML = jsonObj["resource"]["type"];
@@ -788,5 +1238,29 @@ function handleStatusPatch(status, jsonObj)
 	else
 	{
 		getStatus(0, handleStatus_Details);
+	}
+}
+
+
+function play(e)
+{
+	var split = e.target.id.split('_');
+	var type = split[1];
+	var uid = split[2];
+				
+	type = type.substring(0, type.length-1);	//remove the s
+				
+	var play = { "command" : "play", "type" : type, "uid" : uid, "times_to_play" : 1, "shuffle" : false};
+	
+	ajaxPatch(0, "/x-epi/status", JSON.stringify(play), handleStatusPatch);
+				
+}
+
+
+function clearElement(elm)
+{
+	while(elm.firstChild)
+	{
+		elm.removeChild(elm.lastChild);
 	}
 }
