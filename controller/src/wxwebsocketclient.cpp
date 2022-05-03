6601de4fae2d426892305f90e3a035122b5f5f9b
Matthew Martin <martim01@outlook.com>
2022-05-03 08:31:27 +0100

    improvement: update to reconnect

--- controller/src/wxwebsocketclient.cpp
+++ controller/src/wxwebsocketclient.cpp
@@ -1,27 +1,32 @@
 #include "wxwebsocketclient.h"
 #include "log.h"
+#include "json/json.h"
 
 using namespace std::placeholders;
 
 wxDEFINE_EVENT(wxEVT_WS_CONNECTION, wxCommandEvent);
 wxDEFINE_EVENT(wxEVT_WS_MESSAGE, wxCommandEvent);
 
+
+wxWebSocketClient& wxWebSocketClient::Get()
+{
+    static wxWebSocketClient ws;
+    return ws;
+}
+
 wxWebSocketClient::wxWebSocketClient() :
     m_client(std::bind(&wxWebSocketClient::ConnectionCallback, this, _1, _2), std::bind(&wxWebSocketClient::MessageCallback, this, _1,_2))
 {
-
+    Json::StreamWriterBuilder builder;
+    builder["commentStyle"] = "None";
+    builder["indentation"] = "";
+    m_pWriter = std::unique_ptr<Json::StreamWriter>(builder.newStreamWriter());
 }
 
-void wxWebSocketClient::AddHandler(wxEvtHandler* pHandler)
+wxWebSocketClient::~wxWebSocketClient()
 {
-    m_setHandlers.insert(pHandler);
+    Stop();
 }
-
-void wxWebSocketClient::RemoveHandler(wxEvtHandler* pHandler)
-{
-    m_setHandlers.erase(pHandler);
-}
-
 
 void wxWebSocketClient::Run()
 {
@@ -33,39 +38,56 @@ void wxWebSocketClient::Stop()
     m_client.Stop();
 }
 
-bool wxWebSocketClient::Connect(const endpoint& theEndpoint)
+bool wxWebSocketClient::Connect(const endpoint& theEndpoint, wxEvtHandler* pHandler)
 {
-    return m_client.Connect(theEndpoint);
+    if(m_client.Connect(theEndpoint))
+    {
+        m_mHandlers.insert({theEndpoint, pHandler});
+        return true;
+    }
+    return false;
 }
 
 bool wxWebSocketClient::ConnectionCallback(const endpoint& theEndpoint, bool bConnected)
 {
-    int nId = wxNewId();
-    m_mId.insert({theEndpoint, nId});
-    for(auto pHandler : m_setHandlers)
+    auto itHandler = m_mHandlers.find(theEndpoint);
+    if(itHandler != m_mHandlers.end())
     {
         auto pEvent = new wxCommandEvent(wxEVT_WS_CONNECTION);
         pEvent->SetString(theEndpoint.Get());
-        pEvent->SetInt(nId);
         pEvent->SetExtraLong(bConnected);
-        wxQueueEvent(pHandler, pEvent);
+        wxQueueEvent(itHandler->second, pEvent);
     }
     return true;
 }
 
 bool wxWebSocketClient::MessageCallback(const endpoint& theEndpoint, const std::string& sMessage)
 {
-
-    auto itId = m_mId.find(theEndpoint);
-    if(itId != m_mId.end())
+    auto itHandler = m_mHandlers.find(theEndpoint);
+    if(itHandler != m_mHandlers.end())
     {
-        for(auto pHandler : m_setHandlers)
-        {
-            auto pEvent = new wxCommandEvent(wxEVT_WS_MESSAGE);
-            pEvent->SetString(sMessage);
-            pEvent->SetId(itId->second);
-            wxQueueEvent(pHandler, pEvent);
-        }
+        auto pEvent = new wxCommandEvent(wxEVT_WS_MESSAGE);
+        pEvent->SetString(sMessage);
+        wxQueueEvent(itHandler->second, pEvent);
     }
     return true;
+}
+
+
+void wxWebSocketClient::SendAuthentication(const endpoint& theEndpoint, const wxString& sUser, const wxString& sPassword)
+{
+    Json::Value jsData;
+    jsData["action"] = "_authentication";
+    jsData["user"] = sUser.ToStdString();
+    jsData["password"] = sPassword.ToStdString();
+
+    std::stringstream ss;
+    m_pWriter->write(jsData, &ss);
+    pmlLog() << "Sending " << ss.str();
+    m_client.Send(theEndpoint, ss.str());
+}
+
+void wxWebSocketClient::CloseConnection(const endpoint& theEndpoint)
+{
+    m_client.CloseConnection(theEndpoint);
 }
